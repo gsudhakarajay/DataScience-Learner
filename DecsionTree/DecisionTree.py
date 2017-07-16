1debe01e8a516f315220d866d0e34f6ee2807e22
g.sudhakar <g.sudhakar_rao@yahoo.co.in>
2017-07-16 09:50:35 +0530

    updated the files

--- DecsionTree/DecisionTree.py
+++ DecsionTree/DecisionTree.py
@@ -1,1 +1,12 @@
-print "My first Decision Tree"
+from sklearn import tree
+
+#[Height, Weight, Shoesize]
+X = [[181, 80, 44],[177,78,42]]
+Y = ['male', 'female']
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([[181, 80, 43]])
+
+print (prediction)
